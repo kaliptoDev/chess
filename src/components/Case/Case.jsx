5b67d07bc2b6bb704d80d90767e85bf75d049d78
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-09-26 13:18:06 +0200

    Added Green dots for highlight

--- src/components/Case/Case.jsx
+++ src/components/Case/Case.jsx
@@ -1,25 +1,26 @@
 import './Case.css'
 import useCurrentChessboardState from '../../hooks/useCurrentChessboardState'
+import { useState } from 'react'
 
 const Case = ({ children }) => {
 
     const { currentChessboardState, setCurrentChessboardState } = useCurrentChessboardState()
-
-    // const color = "lime"
-    const color = "none"
+    const [backgroundColor, setBackgroundColor] = useState('')
+    const [dotColor, setDotColor] = useState('lime')
 
     const xCoordinate = children[1].props.children
     const yCoordinate = children[0].props.children
 
     const coordinates = xCoordinate + yCoordinate
 
-    // console.log(children)
     return (
         <div className='case'>
-            <div className={`case__bg ${color}`}></div>
+            <div className={`case__bg ${backgroundColor}`}></div>
+            <div className={`case__dot ${dotColor}`}></div>
             {
                 currentChessboardState[coordinates] &&
                 <img
+                    className='case__piece'
                     src={`./assets/${currentChessboardState[coordinates]}.svg`}
                     alt={`${currentChessboardState[coordinates] ? currentChessboardState[coordinates]
                         : "Piece"}`} />
