13a38ea5c8ab98d0fe41d99665dd9ad1ce98b035
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-09-26 13:17:44 +0200

    Added Highlited Provider Provides a map of which cells must be highlighted

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import CurrentLanguageProvider from './providers/CurrentLanguageProvider.jsx'
 import CurrentChessboardProvider from './providers/CurrentChessboardStateProvider.jsx'
+import CurrentChessboardHighlightedProvider from './providers/CurrentChessboardHighlightedProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CurrentLanguageProvider>
       <CurrentChessboardProvider>
-        <Router>
-          <App />
-        </Router>
+        <CurrentChessboardHighlightedProvider>
+          <Router>
+            <App />
+          </Router>
+        </CurrentChessboardHighlightedProvider>
       </CurrentChessboardProvider>
     </CurrentLanguageProvider>
   </React.StrictMode>
