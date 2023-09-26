a2165baf7dbc48781531980b6d08912f7b8a7a43
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-09-26 08:38:42 +0200

    Set CurrentChessboardProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import CurrentLanguageProvider from './providers/CurrentLanguageProvider.jsx'
+import CurrentChessboardProvider from './providers/CurrentChessboardStateProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CurrentLanguageProvider>
-      <Router>
-        <App />
-      </Router>
+      <CurrentChessboardProvider>
+        <Router>
+          <App />
+        </Router>
+      </CurrentChessboardProvider>
     </CurrentLanguageProvider>
   </React.StrictMode>,
 )
--- src/providers/CurrentChessboardStateProvider.jsx
+++ src/providers/CurrentChessboardStateProvider.jsx
@@ -3,7 +3,7 @@ import { useState, createContext, useEffect } from 'react'
 
 // CONTEXT = definition et/ou valeurs par default
 export const CurrentChessboardStateContext = createContext({
-    currentChessboardState: '',
+    currentChessboardState: {},
     setCurrentChessboardState: () => { }
 })
 
@@ -13,7 +13,76 @@ const CurrentChessboardProvider = ({ children }) => {
     const [currentChessboardState, setCurrentChessboardState] = useState('fr');
 
     useEffect(() => {
-        
+        setCurrentChessboardState(
+            {
+                "A1": "white_rook",
+                "B1": "white_knight",
+                "C1": "white_bishop",
+                "D1": "white_queen",
+                "E1": "white_king",
+                "F1": "white_bishop",
+                "G1": "white_knight",
+                "H1": "white_rook",
+                "A2": "white_pawn",
+                "B2": "white_pawn",
+                "C2": "white_pawn",
+                "D2": "white_pawn",
+                "E2": "white_pawn",
+                "F2": "white_pawn",
+                "G2": "white_pawn",
+                "H2": "white_pawn",
+                "A3": null,
+                "B3": null,
+                "C3": null,
+                "D3": null,
+                "E3": null,
+                "F3": null,
+                "G3": null,
+                "H3": null,
+                "A4": null,
+                "B4": null,
+                "C4": null,
+                "D4": null,
+                "E4": null,
+                "F4": null,
+                "G4": null,
+                "H4": null,
+                "A5": null,
+                "B5": null,
+                "C5": null,
+                "D5": null,
+                "E5": null,
+                "F5": null,
+                "G5": null,
+                "H5": null,
+                "A6": null,
+                "B6": null,
+                "C6": null,
+                "D6": null,
+                "E6": null,
+                "F6": null,
+                "G6": null,
+                "H6": null,
+                "A7": "black_pawn",
+                "B7": "black_pawn",
+                "C7": "black_pawn",
+                "D7": "black_pawn",
+                "E7": "black_pawn",
+                "F7": "black_pawn",
+                "G7": "black_pawn",
+                "H7": "black_pawn",
+                "A8": "black_rook",
+                "B8": "black_knight",
+                "C8": "black_bishop",
+                "D8": "black_queen",
+                "E8": "black_king",
+                "F8": "black_bishop",
+                "G8": "black_knight",
+                "H8": "black_rook"
+            }
+        )
+
+        localStorage.setItem('currentChessboardState', JSON.stringify(currentChessboardState));
     }, [])
 
     return (
