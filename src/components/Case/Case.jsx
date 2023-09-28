c2ce2c090f77c6e36722c44ffcf5c3d97c57e984
kaliptoDev <121462067+kaliptoDev@users.noreply.github.com>
2023-09-28 02:38:38 +0200

    Added handling functions for Case
    Functions allow all utils functions to be called when needed
    (not all of them are working for now, esp. the setCurrentChessboardHL)

--- src/components/Case/Case.jsx
+++ src/components/Case/Case.jsx
@@ -3,6 +3,9 @@ import useCurrentChessboardState from '../../hooks/useCurrentChessboardState'
 import useCurrentChessboardHighlightedBlack from '../../hooks/useCurrentChessboardHighlightedBlack'
 import useCurrentChessboardHighlightedWhite from '../../hooks/useCurrentChessboardHighlightedWhite'
 import { useEffect, useState } from 'react'
+import useCurrentSelectedPiece from '../../hooks/useCurrentSelectedPiece'
+import useCurrentColor from '../../hooks/useCurrentColor'
+import { getPossibleMoves } from '../../utils/utils_chessboard'
 
 const Case = ({ children }) => {
 
@@ -14,20 +17,61 @@ const Case = ({ children }) => {
 
     const xCoordinate = children[1].props.children
     const yCoordinate = children[0].props.children
-
+    let dotColorWhite = ''
+    let dotColorBlack = ''
     const coordinates = xCoordinate + yCoordinate
-    let dotColorWhite = currentChessboardHighlightedWhite[coordinates] != false ? 'lime' : ''
-    let dotColorBlack = currentChessboardHighlightedBlack[coordinates] != false ? 'lime' : ''
+
+    if (currentChessboardHighlightedWhite != undefined) {
+        if (currentChessboardHighlightedWhite[coordinates] != undefined && currentChessboardHighlightedBlack[coordinates] != null && currentChessboardHighlightedBlack[coordinates] != '') {
+            dotColorWhite = currentChessboardHighlightedWhite[coordinates] != false ? 'lime' : ''
+            dotColorBlack = currentChessboardHighlightedBlack[coordinates] != false ? 'lime' : ''
+        }
+    }
+
+    const { currentColor, setCurrentColor } = useCurrentColor()
+    const { currentSelectedPiece, setCurrentSelectedPiece } = useCurrentSelectedPiece()
+
+    const handleCaseClick = () => {
+        if (currentChessboardState[coordinates] !== null) {
+            if (currentChessboardState[coordinates].split('_')[0] === currentColor) {
+                setCurrentSelectedPiece(coordinates + '-' + currentChessboardState[coordinates])
+            }
+        }
+        else {
+            setCurrentSelectedPiece('')
+        }
+
+        handleHighlight(coordinates + '-' + currentChessboardState[coordinates])
+
+    }
+
+    const handleHighlight = (currentPiece) => {
+
+        if (currentSelectedPiece != undefined && currentPiece != null && currentPiece != '') {
+            let coords = currentPiece.split('-')[0]
+            let piece = currentPiece.split('-')[1]
+            // console.log(coords, piece)
+            const highlightMap = getPossibleMoves(piece, coords, currentChessboardState, setCurrentChessboardState)
+            if (currentColor === 'white') {
+                setCurrentChessboardHighlightedWhite(highlightMap)
+            }
+            else {
+                setCurrentChessboardHighlightedBlack(highlightMap)
+            }
+        }
+    }
+
 
 
 
     return (
-        <div className='case'>
+        <div className='case' onClick={handleCaseClick}>
             <div className={`case__bg ${backgroundColor}`}></div>
             <div className={`case__dot ${dotColorWhite}`}></div>
             {
                 currentChessboardState[coordinates] &&
                 <img
+
                     className='case__piece'
                     src={`./assets/${currentChessboardState[coordinates]}.svg`}
                     alt={`${currentChessboardState[coordinates] ? currentChessboardState[coordinates]
